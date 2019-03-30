3db3610ceb8a572af19071fc1b52417e1da2522a
Geanie Blanco <geanieblanco@gmail.com>
2019-03-30 19:34:44 -0400

    ballmer peak

---
+++ assets/pages/statusCard.js
@@ -0,0 +1,15 @@
+class StatusCard extends React.Component{
+render() {
+    return (<div className="col-sm-8 col-md-4">
+            <div className="card">
+            <img src="https://via.placeholder.com/120x140" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <p className="card-title">Bronn of the Blackwater</p>
+                <div className="card-text">
+                <p>Current Status: Riding north with Jaime Lannister.</p>
+                </div>
+            </div>
+            </div>
+        </div>)}}
+
+export default StatusCard;
