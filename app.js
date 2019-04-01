89fd675966b1a139f2d75060d3db6edbdcee4fd5
Geanie Blanco <geanieblanco@gmail.com>
2019-04-01 09:44:27 -0400

    card flip

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// document.querySelector(".card-flip").classList.toggle("flip");
+
 var config = {
     apiKey: "AIzaSyCqgkzgZjP5UoW9jrL6FioYwI5k_H86fP0",
     authDomain: "forthethrone-8c01f.firebaseapp.com",
