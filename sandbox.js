91bd66fd0da904a2582d8b0019763ef9d0767695
Geanie Blanco <geanieblanco@gmail.com>
2019-04-04 19:28:39 -0400

    add user complete

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,7 @@
-class FormContainer extends Component {
-
-constructor () {
-    super()
-    this.state = {
-        formControls: {
-            email: {
-              value: ''
-            },
-            username: {
-              value: ''
-            }
-        },
-        userChoices: []
+handleUserLoyalty = (house) => {
+  this.setState({
+    formControls: {
+        loyalty: { house }
     }
-  
-}
-
-changeHandler = event => {
-    
-    const name = event.target.name;
-    const value = event.target.value;
-  
-    this.setState({
-      formControls: {
-          ...this.state.formControls,
-          [name]: {
-          ...this.state.formControls[name],
-          value
-        }
-      }
-    });
-}
-
-
-  render() {
-    return (
-        <div className="form">
-            <input  
-              type="email" 
-              name="email" 
-              value={this.state.formControls.email.value} 
-              onChange={this.changeHandler} 
-            />
-
-            <input 
-              type="text" 
-              name="name" 
-              value={this.state.formControls.name.value} 
-              onChange={this.changeHandler} 
-            />
-        </div>      
-    );
-}
-
+  });
 }
