87f08d0a56866c89a617a9052fbf6e0339cf9aa5
Geanie Blanco <geanieblanco@gmail.com>
2019-04-09 12:42:13 -0400

    firebase update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // document.querySelector(".card-flip").classList.toggle("flip");
+var config = {
+    apiKey: "AIzaSyCqgkzgZjP5UoW9jrL6FioYwI5k_H86fP0",
+    authDomain: "forthethrone-8c01f.firebaseapp.com",
+    databaseURL: "https://forthethrone-8c01f.firebaseio.com",
+    projectId: "forthethrone-8c01f",
+    storageBucket: "forthethrone-8c01f.appspot.com",
+    messagingSenderId: "726563823071"
+    };
+
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
 
 //document references
 userRef = ()=> db.collection("users");
